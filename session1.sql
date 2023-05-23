04fabcd37aaa35b8d41c169e105ef5412312f468
Max <54333442+DrWurzeli@users.noreply.github.com>
2023-05-23 09:49:18 +0200

    Create session1.sql
    first lesson creating a simple database

---
+++ session1.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE Uni;
+
+USE Uni;
+
+CREATE TABLE StudentAnmeldung(
+	StudentID INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+	StudentVorname VARCHAR(20),
+	StudentNachname VARCHAR(20),
+	StudentStadt VARCHAR(20)
+);
+
+CREATE TABLE Module(
+	ModulID tinyint PRIMARY KEY NOT NULL IDENTITY(1,1),
+	ModulName VARCHAR(20)
+);
+
+ALTER TABLE Module
+ADD Anzahl smallint;
+
+ALTER TABLE Module
+DROP COLUMN Anzahl;
+
+INSERT INTO dbo.StudentAnmeldung(StudentVorname, StudentNachname, StudentStadt)
+VALUES
+('Franz', 'Meier', 'Duesseldorf'),
+('Max', 'Mustermann', 'Koeln')
+;
+
+SELECT *
+FROM Uni.dbo.StudentAnmeldung;
